ced05907dfda235cac35034cf9b1615cb033da23
Steve Engledow <steve@offend.me.uk>
2017-04-27 12:47:49 +0100

    Remove deploy script and rename build.py to bleng.py

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SITES="offend.me.uk engledow.me"
-
-now=$(date +%s)
-
-for site in $SITES; do
-    s3cmd sync -P --delete-removed --no-mime-magic --cf-invalidate dist/$site/ s3://$site/
-done
